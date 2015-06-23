5d76c75a9ebe433e6c23233e5d5c0d08da330beb
fabienvuilleumier <fabien.vuilleum@IG13-VUILLEUMF.intra.eiaj.ch>
2015-06-23 13:41:08 +0200

    Ajout du service groupe

---
+++ src/main/webapp/components/services/group-service.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+    var app = angular.module('Fablab');
+    app.factory('GroupService', function ($log, $resource, $http) {
+        var group = $resource(App.API.GROUP_API + "/:id", {id: '@id'});
+        return {
+            list: function (successFn) {
+                $http(
+                        {
+                            method: 'GET',
+                            url: App.API.GROUP_API
+                        }
+                ).success(successFn);
+            },
+            remove: function (id, successFn) {
+                $log.debug("GroupService: remove...");
+                group.remove({id: id}, successFn);
+            },
+            save: function (groupParam, successFn, errorFn) {
+                $log.debug("GroupService: save...");
+                var saved = group.save(groupParam, successFn, errorFn);
+                return saved;
+            },
+            softRemove: function (id, successFn) {
+                $http.get(App.API.GROUP_API + "/softRemove?id=" + id).success(successFn);
+                $log.debug("GroupService: soft remove...");
+            },
+            get: function (id, successFn) {
+                $log.debug("GroupService: get...");
+                var groupRes = group.get({id: id}, successFn);
+                return groupRes;
+            }
+        };
+    });
+}());
+
