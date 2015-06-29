3aff31be6698bc2eb08c9f5956a3784fbaa21027
fabienvuilleumier <fabien.vuilleum@IG13-VUILLEUMF.intra.eiaj.ch>
2015-06-30 00:27:20 +0200

    Mise en place du calendrier (j'ai fait que ça marche)

--- src/main/java/net/collaud/fablab/api/dao/ReservationRepository.java
+++ src/main/java/net/collaud/fablab/api/dao/ReservationRepository.java
@@ -3,7 +3,6 @@ package net.collaud.fablab.api.dao;
 import java.util.Date;
 import java.util.List;
 import net.collaud.fablab.api.data.ReservationEO;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
